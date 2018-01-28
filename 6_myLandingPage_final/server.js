94c1a8c45a7a907ca965a3bec105f33974a7042b
Matheesan <mathee25@hotmail.com>
2018-01-28 01:41:30 -0500

    dfsf

--- 6_myLandingPage_final/server.js
+++ 6_myLandingPage_final/server.js
@@ -6,6 +6,11 @@ app.use(express.static(__dirname + "/public"));
 app.get('/', function (request,response){
 response.render('index.ejs');
 });
+
+app.get('/login', function(request, response){
+response.render('main.ejs');    
+});
+
 app.listen(3000,function(){
     console.log("First API running on port 3000");
     console.log(__dirname);
